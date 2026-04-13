f54591cd11a69041a40f0135e21b725724aae3a7
Ellie-Cunningham <elliecunningham627@gmail.com>
2026-04-13 11:46:34 -0400

    Added navbar.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,49 @@
-<template>
-  <div id="app">
-    <router-view />
-  </div>
-</template>
-
 <script>
+import { RouterLink, RouterView } from 'vue-router'
+import AppLogo from './components/AppLogo.vue'
 export default {
+  components: {
+    AppLogo
+  },
   name: 'App'
 }
 </script>
 
+<template>
+  <div id="app">
+    <nav>
+      <AppLogo />
+      <RouterLink to="/">Calendar</RouterLink>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
 <style>
 body {
   background-color: var(--code-bg);
+}
+
+nav {
+  display: flex;
+  background-color: var(--social-bg);
+  padding: 12px;
+  margin: 12px;
+  border-radius: 30px;
+  border-width: 1px;
+  border-color: var(--border);
+  text-align: left;
+}
+
+nav a {
+  font-weight: bold;
+  color: var(--text-h);
+  text-decoration: none;
+  margin: 2px 10px;
+}
+
+nav a:hover {
+  color: var(--accent);
 }
 
 #app {
@@ -22,7 +53,6 @@ body {
   text-align: center;
   color: #2c3e50;
   background-color: var(--bg);
-  margin-top: 60px;
   width: 75%;
 }
 </style>
---
+++ client/src/components/AppLogo.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+<p>
+    <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="3" y="4" width="18" height="18" rx="2" ry="2" stroke="currentColor" stroke-width="2" fill="none"/>
+        <line x1="16" y1="2" x2="16" y2="6" stroke="currentColor" stroke-width="2"/>
+        <line x1="8" y1="2" x2="8" y2="6" stroke="currentColor" stroke-width="2"/>
+        <line x1="3" y1="10" x2="21" y2="10" stroke="currentColor" stroke-width="2"/>
+    </svg>
+    Exercise Tracker
+</p>
+</template>
+
+<style scoped>
+</style>
