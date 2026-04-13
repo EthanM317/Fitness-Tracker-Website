58590a9fa33ede63d96723ae0bc480b6525f101a
Ellie-Cunningham <elliecunningham627@gmail.com>
2026-04-13 12:47:39 -0400

    Layout changes. Minimal dashboard placeholder.

---
+++ client/src/components/DashboardCounter.vue
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<template>
+    <div class="container">
+        <p>MetricName</p>
+        <h5>Count</h5>
+    </div>
+</template>
+
+<style scoped>
+    .container {
+        background-color: var(--code-bg);
+        border-radius: 12px;
+        padding: 8px 16px;
+        text-align: start;
+    }
+
+    p {
+        font-size: 14px;
+    }
+
+    h5 {
+        color: var(--text-h);
+        margin: 0px;
+    }
+</style>
---
+++ client/src/components/MonthlyDashboard.vue
@@ -0,0 +1,49 @@
+<script>
+    import DashboardCounter from './DashboardCounter.vue';
+
+    export default {
+        components: {
+            DashboardCounter
+        }
+    }
+</script>
+
+<template>
+    <div class="component-container">
+        <h2 id="title">Monthly Overview</h2>
+        <div class="chart-placeholder"></div>
+        <div class="counter-bar">
+            <DashboardCounter />
+            <DashboardCounter />
+            <DashboardCounter />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .component-container {
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+    }
+
+    .counter-bar {
+        display: flex;
+        align-items: flex-start;
+        gap: 10px;
+    }
+
+    .chart-placeholder {
+        background-color: var(--code-bg);
+        border-radius: 12px;
+        padding: 280px 0px;
+    }
+
+    #title {
+        font-size: 36px;
+        background-color: var(--code-bg);
+        border-radius: 12px;
+        padding: 8px 16px;
+        margin: 0px;
+    }
+</style>
